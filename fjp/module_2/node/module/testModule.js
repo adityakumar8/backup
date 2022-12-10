0065c4dc9ef5b4d4d92f5ba34074bf79eb411a1e
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-12-10 17:21:18 +0530

    created own module

---
+++ fjp/module_2/node/module/testModule.js
@@ -0,0 +1,6 @@
+let calc = require("./calculator.js");
+
+console.log(calc.addition(4,5));
+console.log(calc.multiplication(4,5));
+console.log(calc.division(4,5));
+console.log(calc.substraction(4,5));
